f7d34c76159a7789ee89bf4a1d5ee9dcf567a12e
RohitSParadkar <raw123para@gmail.com>
2023-08-16 22:09:33 +0530

    python shell for queries

--- home/views.py
+++ home/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render,HttpResponse
 from datetime import datetime
 from home.models import Contacts
+from django.contrib import messages
 contextText = {
      'title':'CryptoWeb'
 }
 
 # Create your views here.
 def index(request):
+    #messages.success(request, "Profile details updated.")
     return render(request,'index.html',contextText)
     # return HttpResponse('this is home')
 
@@ -17,6 +19,7 @@ def services(request):
     return render(request,'services.html')
 
 def contact(request):
+    #make migrations: create changes
     #logic for form submission
     if request.method=='POST':
         id = request.POST.get("id")
@@ -24,4 +27,5 @@ def contact(request):
         message = request.POST.get("message")
         contact = Contacts(email=email,message=message,date=datetime.today())#save above extracted values in database
         contact.save()
+        messages.success(request, "Form Successfully Submited")
     return render(request,'contactUs.html')
