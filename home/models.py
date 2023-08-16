7c3328ac3ebd7f283cfaeef26b1aa8931668edbf
RohitSParadkar <raw123para@gmail.com>
2023-08-16 10:56:11 +0530

    models registrations and migration settings

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Contacts(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    email = models.CharField(max_length=50)
+    message = models.TextField()
+    date = models.DateField()
